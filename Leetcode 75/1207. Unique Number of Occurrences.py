230ff69000df633807722b049fa0f89ec556b00c
Fashad Ahmed <fashad.ahmed20@gmail.com>
2026-04-30 21:04:50 +0500

    feat: Add solution for unique number of occurrences problem

---
+++ Leetcode 75/1207. Unique Number of Occurrences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        freq = {}
+
+        for num in arr:
+            freq[num] = freq.get(num, 0) + 1
+
+        values = list(freq.values())
+
+        return len(values) == len(set(values))
